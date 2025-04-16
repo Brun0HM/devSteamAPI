4a20a67288506b060ab387241a4e8cafc98ab5aa
Brun0 <brunodev41@gmail.com>
2025-04-16 11:57:55 -0300

    Adicionar arquivos de projeto.

---
+++ Data/APIContext.cs
@@ -0,0 +1,22 @@
+﻿using devSteamAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace devSteamAPI.Data
+{
+
+    public class APIContext : IdentityDbContext<Usuario>
+    {
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options)
+        {
+        }
+        //DbSet
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //Tabeas
+        }
+    }
+}
